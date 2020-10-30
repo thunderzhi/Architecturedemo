6a8fcbb3d75008e14cb990d7c1b7c0d9e05a1aed
Eric.Cui <465785021@qq.com>
2020-10-30 15:48:46 +0800

    jackson

--- cxzmvc/src/main/java/com/controller/IndexController.java
+++ cxzmvc/src/main/java/com/controller/IndexController.java
@@ -3,6 +3,13 @@ package com.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author cxz
  * @Title:
@@ -10,12 +17,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Description:
  * @date 2020/10/26 15:08
  */
-@Controller
+@RestController
 @RequestMapping("/home")
 public class IndexController {
 
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/index2")
+    public String index2() {
+        return "index2";
+    }
+
+    @RequestMapping("/para")
+    public String para() {
+        return "index2";
+    }
+
+    @RequestMapping("/json")
+    //@ResponseBody
+    public Map<String, String> json(){
+        HashMap json = new HashMap();
+        json.put("success", "XXXXXXXXX");
+        return json;
     }
 }

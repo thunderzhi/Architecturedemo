2483e178e9d789aa5242a6da442631765c6fc0f5
Eric.Cui <465785021@qq.com>
2020-11-02 15:29:03 +0800

    issue  NoSuchBeanDefinitionException: No qualifying bean of type 'org.springframework.data.redis.core.RedisTemplate<?, ?>' available: expected at least 1 bean which qualifies as autowire candidate. perhaps web.xml miss listener

---
+++ cxzmvc/src/main/java/com/cxz/impl/TestService.java
@@ -0,0 +1,20 @@
+package com.cxz.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/11/2 11:04
+ */
+@Service
+public class TestService {
+
+    public String test(){
+        return "----------------------";
+    }
+
+
+}

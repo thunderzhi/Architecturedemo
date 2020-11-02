98bc5af78fe66dbae49232a93da7eb75c31da0ba
Eric.Cui <465785021@qq.com>
2020-11-02 18:07:12 +0800

    redis serialize

--- cxzmvc/src/main/java/com/cxz/controller/IndexController.java
+++ cxzmvc/src/main/java/com/cxz/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cxz.controller;
 
 import com.cxz.impl.RedisService;
 import com.cxz.impl.TestService;
+import com.cxz.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +62,33 @@ public class IndexController {
         //Timestamp time1 = new Timestamp(System.currentTimeMillis());
         //redisService.redisUtil2.set(key,dateStr);
         json.put("success", key);
+        return json;
+    }
+
+    @RequestMapping("/getrk")
+    //@ResponseBody
+    public Map<String, String> GetRandomkey(String k){
+        HashMap json = new HashMap();
+        //String dateStr = Long.toString(System.currentTimeMillis()/1000L);
+        //String key = "cxzmvc"+dateStr;
+        //Timestamp time1 = new Timestamp(System.currentTimeMillis());
+        Object v = redisService.redisUtil2.get(k);
+        json.put("success", v);
+        return json;
+    }
+
+    @RequestMapping("/add")
+    //@ResponseBody
+    public Map<String, String> adduser(String k){
+        User u = new User();
+        HashMap json = new HashMap();
+        String dateStr = Long.toString(System.currentTimeMillis()/1000L);
+        u.setAge(dateStr);
+        u.setName(k);
+        //String key = "cxzmvc"+dateStr;
+        //Timestamp time1 = new Timestamp(System.currentTimeMillis());
+        boolean b = redisService.redisUtil2.set(k,u);
+        json.put("success", String.valueOf(b));
         return json;
     }
 }

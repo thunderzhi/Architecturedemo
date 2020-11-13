bd151e1d34be1ec9502dd78e7228ef381872a977
Eric.Cui <465785021@qq.com>
2020-11-13 10:52:00 +0800

    extract

--- cxzmvc/src/main/java/com/cxz/impl/RedisService.java
+++ cxzmvc/src/main/java/com/cxz/impl/RedisService.java
@@ -15,44 +15,5 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RedisService {
-    @Autowired
-    public RedisUtil redisUtil;
 
-    public boolean set(String key, Object value){
-        return this.redisUtil.set(  key, value);
-    }
-    /*设置字符串kv*/
-    public boolean setStr(String key, Object value) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonstr = objectMapper.writeValueAsString(value);
-        return this.redisUtil.setStr(  key, jsonstr);
-    }
-
-    /*设置字符串kv,超时时间*/
-    public boolean setStr(String key, Object value,long time) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonstr = objectMapper.writeValueAsString(value);
-        return this.redisUtil.setStr(  key, jsonstr,time);
-    }
-
-    /*设置原子自增减*/
-    public long incr(String key, long delta){
-        return this.redisUtil.incr(key, delta);
-    }
-
-    /*设置原子自增减*/
-    public long decr(String key, long delta){
-        return this.redisUtil.decr(key, delta);
-    }
-
-    /*锁*/
-    public boolean lock(String key, String value, long milliseconds){
-        return this.redisUtil.lock2(key, value,milliseconds);
-    }
-    public boolean unlock(String key, String value){
-        return this.redisUtil.releaselock2(key, value);
-    }
-    public long exp(String key){
-        return this.redisUtil.getStrExpire(key);
-    }
 }

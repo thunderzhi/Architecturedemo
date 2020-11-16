8bf8779e75040fc26b76fe880d7712935f7e5f7b
Eric.Cui <465785021@qq.com>
2020-11-16 22:57:29 +0800

    cloudqueue

---
+++ cloudqueue/src/test/java/com/cxz/cloudqueue/CloudqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxz.cloudqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudqueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

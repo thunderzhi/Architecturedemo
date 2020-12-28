ce1793624de1cf6adf57b5d987f061e7105f81fc
Eric.Cui <465785021@qq.com>
2020-12-28 22:03:31 +0800

    mybatis.generator

---
+++ codegenerator/src/main/java/com/cxz/codegenerator/CodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.cxz.codegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodegeneratorApplication.class, args);
+    }
+
+}

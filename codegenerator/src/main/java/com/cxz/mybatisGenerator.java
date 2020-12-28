cbc9e4c5f33390523dfdfcd87e591556693bb184
Eric.Cui <465785021@qq.com>
2020-12-29 00:43:41 +0800

    gen

---
+++ codegenerator/src/main/java/com/cxz/mybatisGenerator.java
@@ -0,0 +1,34 @@
+package com.cxz;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/12/28 22:39
+ */
+public class mybatisGenerator {
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("D:\\dev\\project\\ArchitectureProj\\Architecturedemo\\codegenerator\\src\\main\\resources\\mybatis-generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}

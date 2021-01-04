4615279348a6661f2f7b9b281a832bfb95cee4ad
Eric.Cui <465785021@qq.com>
2021-01-04 21:37:00 +0800

    mapper

---
+++ cxzmvc/src/main/java/com/cxz/mapper/TUserMapper.java
@@ -0,0 +1,32 @@
+package com.cxz.mapper;
+
+
+import com.cxz.model.TUser;
+import com.cxz.model.TUserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TUserMapper {
+    long countByExample(TUserExample example);
+
+    int deleteByExample(TUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TUser record);
+
+    int insertSelective(TUser record);
+
+    List<TUser> selectByExample(TUserExample example);
+
+    TUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TUser record, @Param("example") TUserExample example);
+
+    int updateByExample(@Param("record") TUser record, @Param("example") TUserExample example);
+
+    int updateByPrimaryKeySelective(TUser record);
+
+    int updateByPrimaryKey(TUser record);
+}

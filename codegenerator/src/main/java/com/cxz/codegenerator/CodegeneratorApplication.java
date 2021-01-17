0b960e93aed7994716ef59bf2a0fddda16c4338a
Eric.Cui <465785021@qq.com>
2021-01-17 15:16:34 +0800

    application.properties

--- codegenerator/src/main/java/com/cxz/codegenerator/CodegeneratorApplication.java
+++ codegenerator/src/main/java/com/cxz/codegenerator/CodegeneratorApplication.java
@@ -1,9 +1,11 @@
 package com.cxz.codegenerator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cxz.mapper")
 public class CodegeneratorApplication {
 
     public static void main(String[] args) {

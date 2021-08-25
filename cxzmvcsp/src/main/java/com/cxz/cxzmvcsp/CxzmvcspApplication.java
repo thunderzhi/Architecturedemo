7e8536e831b45762ad03f4210a7728907b34f4b1
Eric.Cui <465785032@qq.com>
2021-08-25 10:36:10 +0800

    new

---
+++ cxzmvcsp/src/main/java/com/cxz/cxzmvcsp/CxzmvcspApplication.java
@@ -0,0 +1,13 @@
+package com.cxz.cxzmvcsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxzmvcspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxzmvcspApplication.class, args);
+    }
+
+}
---
+++ cxzmvcsp/src/test/java/com/cxz/cxzmvcsp/CxzmvcspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxz.cxzmvcsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxzmvcspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

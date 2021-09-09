9237ff7a81c0a1c86e04201360cecd08be2fc03f
Eric.Cui <465785032@qq.com>
2021-09-09 14:21:27 +0800

    test

--- cxzmvc/src/main/java/com/cxz/algorithservice/test958.java
+++ cxzmvc/src/main/java/com/cxz/algorithservice/test958.java
@@ -1,5 +1,8 @@
 package com.cxz.algorithservice;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author cxz
  * @Title:
@@ -8,4 +11,45 @@ package com.cxz.algorithservice;
  * @date 2021/9/9 11:35
  */
 public class test958 {
+    class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isCompleteTreeBFS(TreeNode root){
+        if(root==null){
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        boolean reachnull = false;
+        queue.offer(root);
+        while(!queue.isEmpty()){
+
+            TreeNode tmp = queue.poll();
+            if (tmp==null){
+                reachnull = true;
+            }
+            else {
+                if(reachnull){
+                    return false;
+                }
+
+                System.out.println("Poll "+tmp.val);
+                TreeNode left = tmp.left;
+                TreeNode right = tmp.right;
+                queue.offer(left);
+                queue.offer(right);
+            }
+
+        }
+        return true;
+    }
+
 }
